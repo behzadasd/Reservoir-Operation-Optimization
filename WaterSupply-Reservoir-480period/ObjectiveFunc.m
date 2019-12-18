d8bcddda5e7c71a16a15e1774670611981359e13
behzadasd <58999767+behzadasd@users.noreply.github.com>
2019-12-17 21:59:37 -0500

    Update ObjectiveFunc.m

--- WaterSupply-Reservoir-480period/ObjectiveFunc.m
+++ WaterSupply-Reservoir-480period/ObjectiveFunc.m
@@ -1,4 +1,14 @@
 function [x,Penalized_fit,fit]=ObjectiveFunc(x,Inflow,Demand,Loss)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%  Charged System Search (CSS) Optimization Algorithm          %%%
+%%%  Objective Function code - Water-Supply Reservoir Operation  %%%
+%%%        https://www.mdpi.com/2306-5338/6/1/5                  %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Behzad Asadieh, Ph.D.           %%%
+%%% University of Pennsylvania      %%%
+%%% basadieh@sas.upenn.edu          %%%
+%%% github.com/behzadasd            %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Lower bounds
 x_min=0;
